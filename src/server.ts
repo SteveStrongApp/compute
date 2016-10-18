ed81c16c23f55fb4d11c6b66af8cc0f9d3226b07
Steve Strong <sstrong@apprenticesystems.com>
2016-10-17 20:41:53 -0400

    good references

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@
 declare var __dirname;
   
 
-import * as express from 'express';
+import * as express from "express";
 import { handlebars } from  'consolidate'
 import { Router } from 'vio';
 import * as Path from 'path';
@@ -11,15 +11,15 @@ import * as Path from 'path';
 let app = express();
 
 
-//app.engine('hbs', handlebars);
+app.engine('hbs', handlebars);
 
 var myPath = Path.join(__dirname, '../static');
 app.use('/static', express.static(myPath));
 
 let router = new Router(app, {
     routesRoot: Path.join(__dirname,'routes'),
-    viewsRoot: Path.join(__dirname,'../views')
-    //viewsExtensions: 'hbs',
+    viewsRoot: Path.join(__dirname,'../views'),
+    viewsExtension: 'hbs'
 });
 
 const port = 5000;
